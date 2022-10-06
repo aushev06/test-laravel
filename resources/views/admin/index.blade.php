775bff1d0b737753f3a8fb104752502dc56dda20
aushev06 <Rj3-Hnf-tG4-J3L>
2022-10-06 11:06:49 +0300

    INIT ADMIN PANEL

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,20 @@
+@extends('admin.layout.main')
+
+@section('content')
+    <div class="content-wrapper">
+        <div class="container-fluid">
+            <!-- Breadcumbs-->
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item">
+                    <a href="#">Доска</a>
+                </li>
+            </ol>
+
+            <h2></h2>
+        </div>
+        <!-- /.container-fluid-->
+    </div>
+@endsection
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin.index');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('/', [\App\Http\Controllers\Admin\AdminController::class, 'index'])->name('admin.index');
 });
